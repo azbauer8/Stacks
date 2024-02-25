f4552983d6af6bb4a9de34d1b594332ee65c2092
Zach Bauer <azbauer8@gmail.com>
2024-02-25 16:30:53 -0500

    replacing fullpage loading with inline suspense

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,13 @@
 import StackGridWrapper from "@/components/StackCardGrid/StackGridWrapper"
+import { Suspense } from "react"
 import NewUserMsg from "./NewUserMsg"
 
 export default async function Home() {
   return (
     <>
-      <NewUserMsg />
+      <Suspense fallback={<div />}>
+        <NewUserMsg />
+      </Suspense>
       <StackGridWrapper title="Public Stacks" />
     </>
   )
