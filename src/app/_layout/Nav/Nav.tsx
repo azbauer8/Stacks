a04d90c836e464f1c1051caf676a381ffda9e3ab
Zach Bauer <azbauer8@gmail.com>
2024-03-03 20:04:16 -0800

    simplified navbar structure

--- src/app/_layout/Nav/Nav.tsx
+++ src/app/_layout/Nav/Nav.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from "react"
+import { Route } from "next"
 import Image from "next/image"
 import NextLink from "next/link"
+import Link from "next/link"
 import { siteConfig } from "@/config"
-import { NavbarBrand, NavbarContent, Skeleton } from "@nextui-org/react"
+import { getAuthUser } from "@/supabase/queries"
+import { Button, NavbarBrand, NavbarContent, Skeleton } from "@nextui-org/react"
+import { PlusIcon } from "lucide-react"
 
-import NavActions from "./NavActions"
+import { ThemeToggle } from "@/app/_layout/Nav/ThemeToggle"
+import { signIn } from "@/app/auth/actions"
+
+import NavMenu from "./NavMenu"
 import NavWrapper from "./NavWrapper"
+import { SignInButton } from "./SignInButton"
 
 export default function Nav() {
   return (
@@ -39,6 +47,35 @@ function NavActionSkeleton() {
     <div className="flex items-center gap-2.5">
       <Skeleton className="size-8 rounded-lg" />
       <Skeleton className="size-10 rounded-full" />
+    </div>
+  )
+}
+
+async function NavActions() {
+  const user = await getAuthUser()
+  if (user) {
+    return (
+      <>
+        <Button
+          as={Link}
+          isIconOnly
+          size="sm"
+          variant="ghost"
+          aria-label="Theme toggle placeholder"
+          href={"/new" as Route}
+        >
+          <PlusIcon className="size-[1.2rem]" />
+        </Button>
+        <NavMenu user={user} />
+      </>
+    )
+  }
+  return (
+    <div className="flex items-center gap-2">
+      <form action={signIn}>
+        <SignInButton />
+      </form>
+      <ThemeToggle />
     </div>
   )
 }
